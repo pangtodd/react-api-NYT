539fb9cc2530c5951f3c7a1043530923cfb9d217
Todd Pangilinan <pang.todd@gmail.com>
2022-09-11 10:17:44 -0700

    established TopStories.js component.

---
+++ src/components/TopStories.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function TopStories() {
+  const [error, setError] = useState(null);
+  const [isLoad, setIsLoaded]= useState(false);
+  const [topStories, setTopStories] = useState([]);
+}
+
+export default TopStories;

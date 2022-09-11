ea85ae672b99869c9c116b6d1d7f9e633cd3bece
Todd Pangilinan <pang.todd@gmail.com>
2022-09-11 14:34:45 -0700

    established first test, passes. Established 2nd and 3rd tests, both fail positively.

--- src/__tests__/reducers/top-stories-reducer.test.js
+++ src/__tests__/reducers/top-stories-reducer.test.js
@@ -1,6 +1,9 @@
 import topStoriesReducer from '../../reducers/top-stories-reducer';
+import * as c from '../../actions/ActionTypes';
 
 describe('topStoriesReducer', () => {
+
+  let action;
 
   const initialState = {
     isLoaded: false,
@@ -8,11 +11,37 @@ describe('topStoriesReducer', () => {
     error: null
   };
 
+  test('successfully getting top stories should change isLoaded to tru and update topStories', ()=>{
+    const topStories ="An Article";
+    action={
+      type:c.GET_TOP_STORIES_SUCCESSS,
+      topStories
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      topStories: "An article",
+      error: null
+    });
+  });
+
   test('should successfully throw a new error if a non-matching action type is passed into it', () => {
     expect(
       () => {
         topStoriesReducer(initialState, {type: null })
       }
     ).toThrowError("There is no action matching null.");
+  });
+
+  test('requesting topStories should successfully change isLoaded from false to true', ()=> {
+    action ={
+      type: c.REQUEST_TOP_STORIES
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded:true,
+      topStories:[],
+      error: null,
+    });
   });
 });

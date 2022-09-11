2d3affb95df242f7963fd8741af143df8138aef2
Todd Pangilinan <pang.todd@gmail.com>
2022-09-11 10:40:11 -0700

    updated TopStories to make initial API call.

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function TopStories() {
   const [error, setError] = useState(null);
   const [isLoad, setIsLoaded]= useState(false);
   const [topStories, setTopStories] = useState([]);
+
+  useEffect(()=>{
+  fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+    .then(response=>response.json())
+    .then((jsonifiedResponse)=>{
+      setTopStories(jsonifiedResponse.results)
+      setIsLoaded(true)
+    });
+  },[]);
 }
 
 export default TopStories;

4d19fb286a4efba9cb20a6829fec0208617818aa
Todd Pangilinan <pang.todd@gmail.com>
2022-09-12 10:36:21 -0700

    bugfix: typo in Action.Types.js was breaking tests.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -5,14 +5,14 @@ describe('top stories reducer actions', ()=> {
   it('getTopStoriesSuccess should create GET_TOP_STORIES_SUCCESS ACTION', ()=>{
     const topStories= "An article";
     expect(actions.getTopStoriesSuccess(topStories)).toEqual({
-      type:c.GET_TOP_STORIES_SUCCESSS,
+      type: c.GET_TOP_STORIES_SUCCESS,
       topStories
     });
   });
 
   it('getTopStoriesFailure should create GET_TOP_STORIES_FAILURE action',()=>{
     const error = "An error";
-    expect (actions.getTopSToriesFailure(error)).toEqual({
+    expect (actions.getTopStoriesFailure(error)).toEqual({
       type: c.GET_TOP_STORIES_FAILURE,
       error
     });
